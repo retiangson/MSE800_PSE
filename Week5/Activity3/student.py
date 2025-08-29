776aa9bd45f1d43c6b69c989352606cb18e15715
retiangson <116651559+retiangson@users.noreply.github.com>
2025-08-30 10:06:09 +1200

    add comments

--- Week5/Activity3/student.py
+++ Week5/Activity3/student.py
@@ -5,9 +5,10 @@ class Student(Person):
         super().__init__(name, address, age, ID)
         self.academic_record = academic_record
 
+    # Overridden version (hides address)
     def display_info(self):
         return (f"Name: {self.name}, Address: Confidential, "
             f"Age: {self.age}, ID: {self.ID}, Academic Record: {self.academic_record}")
-
+    # Method to call the original parent version explicitly
     def display_original_info(self):
         return super().display_info() + f", Academic Record: {self.academic_record}"
