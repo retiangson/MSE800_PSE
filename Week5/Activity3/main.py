36a47b56341523a3b5cebeabaa2c192f890f19f5
retiangson <116651559+retiangson@users.noreply.github.com>
2025-08-30 09:31:08 +1200

    updates

--- Week5/Activity3/main.py
+++ Week5/Activity3/main.py
@@ -5,9 +5,9 @@ from general_staff import GeneralStaff
 
 if __name__ == "__main__":
     # Create instances
-    student = Student("Alice", "123 University St", 20, "S001", "Excellent")
-    academic = Academic("Dr. Bob", "45 College Rd", 45, "A123", "TX-2024", 90000)
-    staff = GeneralStaff("Charlie", "78 Campus Ln", 35, "G555", "TX-5678", 25.5)
+    student = Student("Ron", "123 University St", 20, "S001", "Excellent")
+    academic = Academic("Hazel", "45 College Rd", 45, "A123", "TX-2024", 90000)
+    staff = GeneralStaff("Celestine", "78 Campus Ln", 35, "G555", "TX-5678", 25.5)
 
     # Display info
     print(student.display_info())
